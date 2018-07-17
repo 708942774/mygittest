05f63798fd3e0219ee6b2dae1f29757e5d622bc4
zhoupeilin <zhoupeilin@hahaha.com>
2018-07-17 08:59:02 +0800

    寡人驾到

---
+++ ssm/src/main/java/com/itheima/service/AccountService.java
@@ -0,0 +1,12 @@
+package com.itheima.service;
+
+import java.util.List;
+
+import com.itheima.domain.Account;
+
+public interface AccountService {
+
+	public List<Account> findAll();
+	
+	public void saveAccount(Account account);
+}
